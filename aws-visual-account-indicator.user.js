f01a90c508a32b76edf0d632f3ccc6b3f1281f31
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-08-31 17:09:10 +0200

    Update aws-visual-account-indicator.user.js

--- aws-visual-account-indicator.user.js
+++ aws-visual-account-indicator.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AWS Visual Account Indicator
 // @namespace    https://qoomon.github.io
-// @version      1.0.2
+// @version      1.0.3
 // @updateURL    https://github.com/qoomon/userscript-aws-visual-account-indicator/raw/main/aws-visual-account-indicator.user.js
 // @downloadURL  https://github.com/qoomon/userscript-aws-visual-account-indicator/raw/main/aws-visual-account-indicator.user.js
 // @description  This userscript reads the aws-userInfo cookie and adds account name and color indicator
 // @author       qoomon
 // @match        https://*.console.aws.amazon.com/*
-// @icon         https://a0.awsstatic.com/libra-css/images/logos/aws_smile-header-desktop-en-white_59x35.png
+// @icon         https://aws.amazon.com/favicon.ico
 // @grant        none
 // ==/UserScript==
 
