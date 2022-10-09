17e1e2aebba72ac87c6a782943ec6f2da9263d8f
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-10-09 15:27:25 +0200

    Update aws-visual-account-indicator.user.js

--- aws-visual-account-indicator.user.js
+++ aws-visual-account-indicator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS Visual Account Indicator
 // @namespace    https://qoomon.github.io
-// @version      1.0.8
+// @version      1.0.9
 // @updateURL    https://github.com/qoomon/userscript-aws-visual-account-indicator/raw/main/aws-visual-account-indicator.user.js
 // @downloadURL  https://github.com/qoomon/userscript-aws-visual-account-indicator/raw/main/aws-visual-account-indicator.user.js
 // @description  This userscript reads the aws-userInfo cookie and adds account name and color indicator
@@ -11,56 +11,25 @@
 // @grant        none
 // ==/UserScript==
 
+// --- Configure display name and color ------------------------------------
+
+function getDisplayName(userInfo) {
+    return userInfo.accountName || userInfo.accountAlias || userInfo.accountId
+}
+
+function getDisplayColor(userInfo) {
+    const displayName = getDisplayName(userInfo)
+    if(displayName.match(/(^|[^a-zA-Z])(production|prod)([^a-zA-Z]|$)/)) return '#921b1d'
+    if(displayName.match(/(^|[^a-zA-Z])(staging|stage)([^a-zA-Z]|$)/)) return '#a27401'
+    if(displayName.match(/(^|[^a-zA-Z])(sandbox|lab|)([^a-zA-Z]|$)/)) return '#016a83'
+    return '#7c7c7c'
+}
+
 (function() {
     'use strict';
 
-    // --- Configure display name and color ------------------------------------
-
-    function getAccountDisplayName(userInfo) {
-        return userInfo.accountName || userInfo.accountAlias || userInfo.accountId
-    }
-
-    function getAccountDisplayColor(userInfo) {
-        const displayName = getAccountDisplayName(userInfo)
-        if(displayName.match(/(^|[^a-zA-Z])(production|prod)([^a-zA-Z]|$)/)) return '#921b1d'
-        if(displayName.match(/(^|[^a-zA-Z])(staging|stage|)([^a-zA-Z]|$)/)) return '#a27401'
-        if(displayName.match(/(^|[^a-zA-Z])(lab|sandbox)([^a-zA-Z]|$)/)) return '#016a83'
-        return '#7c7c7c'
-    }
-
-    // --- Utils ---------------------------------------------------------------
-
-    function getCookie(name) {
-        return document.cookie.split('; ').find(c => c.startsWith(`${name}=`))
-            .replace(/^[^=]+=/, '')
-    }
-
-    function getUserInfo() {
-        const userInfo = JSON.parse(decodeURIComponent(getCookie('aws-userInfo')))
-
-        let identityArn = userInfo.arn
-        let identityDisplayName = decodeURIComponent(userInfo.username).replace(/^[^/]*\//, '')
-        if (identityDisplayName.startsWith('AWSReservedSSO_')) {
-            identityDisplayName = identityDisplayName.replace(/^AWSReservedSSO_/, '').replace(/_[^_]+\//, '/')
-        }
-        let accountId = userInfo.arn.split(':')[4]
-        let accountAlias = userInfo.alias !== accountId ? userInfo.alias : undefined
-        let issuer = decodeURI(userInfo.issuer)
-        let issuerMatch = issuer.match(new RegExp(`/${accountId} \\((?<name>[^)]*)\\)/`))
-        let accountName = issuerMatch ? issuerMatch.groups.name : undefined
-
-        return {
-            identityArn,
-            identityDisplayName,
-            accountId,
-            accountAlias,
-            issuer,
-            accountName,
-        }
-    }
-
     const userInfo = getUserInfo()
-    const indicatorColor = getAccountDisplayColor(userInfo)
+    const indicatorColor = getDisplayColor(userInfo)
 
     // --- Add account label ---------------------------------------------------
 
@@ -70,7 +39,7 @@
     } else {
         accountNameElement.href = userInfo.issuer.split('#')[0]
     }
-    accountNameElement.innerText = getAccountDisplayName(userInfo)
+    accountNameElement.innerText = getDisplayName(userInfo)
     accountNameElement.style.cssText = `
         height: 20px;
         color: white !important;
@@ -101,3 +70,34 @@
     const navHeaderElement = document.querySelector('#awsc-nav-header nav')
     navHeaderElement.appendChild(indicatorBarElement)
 })();
+
+// --- Utils ---------------------------------------------------------------
+
+function getCookie(name) {
+    return document.cookie.split('; ').find(c => c.startsWith(`${name}=`))
+        .replace(/^[^=]+=/, '')
+}
+
+function getUserInfo() {
+    const userInfo = JSON.parse(decodeURIComponent(getCookie('aws-userInfo')))
+
+    let identityArn = userInfo.arn
+    let identityDisplayName = decodeURIComponent(userInfo.username).replace(/^[^/]*\//, '')
+    if (identityDisplayName.startsWith('AWSReservedSSO_')) {
+        identityDisplayName = identityDisplayName.replace(/^AWSReservedSSO_/, '').replace(/_[^_]+\//, '/')
+    }
+    let accountId = userInfo.arn.split(':')[4]
+    let accountAlias = userInfo.alias !== accountId ? userInfo.alias : undefined
+    let issuer = decodeURI(userInfo.issuer)
+    let issuerMatch = issuer.match(new RegExp(`/${accountId} \\((?<name>[^)]*)\\)/`))
+    let accountName = issuerMatch ? issuerMatch.groups.name : undefined
+
+    return {
+        identityArn,
+        identityDisplayName,
+        accountId,
+        accountAlias,
+        issuer,
+        accountName,
+    }
+}
